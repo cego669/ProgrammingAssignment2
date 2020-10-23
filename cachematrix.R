65278ad7e0d04d1e3f80b1fbf4d20b0da13e2f87
Carlos Eduardo <c.e.g.o@hotmail.com>
2020-10-23 20:32:05 -0300

    My code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My functions are basically equal to the functions described in the vector
+## mean example (I still didn't understand the code completely, but I understand
+## more now than before).
 
-## Write a short comment describing this function
+## This function creates a special matrix that can store it's own inverse
+## when applied at least one time in the cacheSolve() function.
 
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function see if the special matrix has any cache. If it has, then there
+## is no computation and the inverse is returned. It computes the inverse other-
+## wise, and return the result.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
